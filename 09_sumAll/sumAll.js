2b93fbff202e66a1523b11a0e7e228c56d9ff3a9
njavg <223289851+Njavg@users.noreply.github.com>
2025-08-28 13:04:58 -0400

    ejercicios arrays

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  const array = [];
+  for (let i = min; i <= max; i++) {
+    array.push(i);
+  }
+  const productOfAllNums = array.reduce((total, currentItem) => {
+  return total + currentItem;
+}, 0);
+return productOfAllNums;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
